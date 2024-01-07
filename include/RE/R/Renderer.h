10652f5f4c039e714553041879bc634e963a40d0
ArranzCNL <52199624+ArranzCNL@users.noreply.github.com>
2024-01-07 17:21:23 +0000

    Added powerof3 commit 32ecbf5 (Renderer)
    Fixes alandtse commit 738afc4

--- include/RE/R/Renderer.h
+++ include/RE/R/Renderer.h
@@ -4,6 +4,7 @@
 #include "RE/N/NiTexture.h"
 #include "RE/R/RenderTargetData.h"
 #include "RE/T/TextureFileFormat.h"
+#include "SKSE/Version.h"
 
 struct ID3D11Device;
 struct ID3D11DeviceContext;
@@ -38,14 +39,14 @@ namespace RE
 		struct RendererData2
 		{
 		public:
-#define RENDERER_DATA2_CONTENT                                                                                     \
-			CubemapRenderTargetData        cubemapRenderTargets[RENDER_TARGET_CUBEMAP::kTOTAL]; /* 26D8, VR 2E48 */ \
-			Texture3DTargetData            texture3DRenderTargets[RENDER_TARGET_3D::kTOTAL];    /* 2718, VR 2E88*/  \
-			float                          clearColor[4];                                       /* 2778, VR 2EE8*/  \
-			std::uint8_t                   clearStencil;                                        /* 2788, VR 2EF8*/  \
-			SKSE::WinAPI::CRITICAL_SECTION lock;                                                /* 2790, VR 2F00*/  \
-			const char*                    className;                                           /* 27B8, VR 2F28*/  \
-			SKSE::WinAPI::HINSTANCE        hInstance;                                           /* 27C0, VR 2F30*/
+#define RENDERER_DATA2_CONTENT                                                                                                   \
+			CubemapRenderTargetData        cubemapRenderTargets[RENDER_TARGET_CUBEMAP::kTOTAL];  /* 26D8, VR 2E48, AE1130 2738 */ \
+			Texture3DTargetData            texture3DRenderTargets[RENDER_TARGET_3D::kTOTAL];     /* 2718, VR 2E88, AE1130 2778 */ \
+			float                          clearColor[4];                                        /* 2778, VR 2EE8, AE1130 27d8 */ \
+			std::uint8_t                   clearStencil;                                         /* 2788, VR 2EF8, AE1130 27e8 */ \
+			SKSE::WinAPI::CRITICAL_SECTION lock;                                                 /* 2790, VR 2F00, AE1130 27f0 */ \
+			const char*                    className;                                            /* 27B8, VR 2F28, AE1130 2818 */ \
+			SKSE::WinAPI::HINSTANCE        hInstance;                                            /* 27C0, VR 2F30, AE1130 2820 */
 
             RENDERER_DATA2_CONTENT
 		};
@@ -53,12 +54,12 @@ namespace RE
 		struct DepthStencilRuntimeData
 		{
 		public:
-#ifndef ENABLE_SKYRIM_VR
-#define DEPTHSTENCIL_RUNTIME_DATA_CONTENT                                                           \
-			DepthStencilData depthStencils[RENDER_TARGET_DEPTHSTENCIL::kTOTAL]; /* 1FB8, VR 21D0 */
+#if !defined(ENABLE_SKYRIM_VR)
+#define DEPTHSTENCIL_RUNTIME_DATA_CONTENT                                                                          \
+			DepthStencilData depthStencils[RENDER_TARGET_DEPTHSTENCIL::kTOTAL];  /* 1FB8, VR 21D0, AE1130 0x2018 */
 #else
-#define DEPTHSTENCIL_RUNTIME_DATA_CONTENT                                                   \
-			DepthStencilData depthStencils[RENDER_TARGET_DEPTHSTENCIL::kVRTOTAL]; /* 1FA8*/
+#define DEPTHSTENCIL_RUNTIME_DATA_CONTENT                                                                            \
+			DepthStencilData depthStencils[RENDER_TARGET_DEPTHSTENCIL::kVRTOTAL];  /* 1FB8, VR 21D0, AE1130 0x2018 */
 #endif
 			DEPTHSTENCIL_RUNTIME_DATA_CONTENT
 		};
@@ -67,56 +68,52 @@ namespace RE
 		{
 		public:
 			// members
-#ifndef ENABLE_SKYRIM_VR
-#define RUNTIME_DATA_CONTENT                                                                      \
-			std::uint32_t        uiAdapter;                             /* 0018 */                 \
-			std::uint32_t        unk04;                                 /* 001C - refreshRate? */  \
-			std::uint32_t        unk08;                                 /* 0020 */                 \
-			std::uint32_t        unk0C;                                 /* 0024 */                 \
-			std::uint32_t        unk10;                                 /* 0028 */                 \
-			std::uint32_t        unk14;                                 /* 002C */                 \
-			std::uint32_t        unk18;                                 /* 0030 */                 \
-			std::uint32_t        unk1C;                                 /* 0034 */                 \
-			bool                 fullScreen;                            /* 0038 */                 \
-			bool                 borderlessDisplay;                     /* 0039 */                 \
-			bool                 readOnlyDepth;                         /* 003A */                 \
-			bool                 unk23;                                 /* 003B */                 \
-			bool                 unk24;                                 /* 003C */                 \
-			bool                 unk25;                                 /* 003D */                 \
-			std::uint32_t        unk28;                                 /* 0038 */                 \
-			std::uint32_t        unk30;                                 /* 0040 */                 \
-			std::uint32_t        presentInterval;                       /* 004C */                 \
-			ID3D11Device*        forwarder;                             /* 0050 */                 \
-			ID3D11DeviceContext* context;                               /* 0058 */                 \
-			RendererWindow       renderWindows[32];                     /* 0060 */                 \
-			RenderTargetData     renderTargets[RENDER_TARGET::kTOTAL];  /* 0A60 */
+#if !defined(ENABLE_SKYRIM_VR)
+#define RUNTIME_DATA_CONTENT                                                                   \
+			std::uint32_t                     adapter;                               /* 0010 */ \
+			DirectX::DXGI_RATIONAL            desiredRefreshRate;                    /* 001C */ \
+			DirectX::DXGI_RATIONAL            actualRefreshRate;                     /* 0020 */ \
+			DirectX::DXGI_MODE_SCALING        scaleMode;                             /* 0024 */ \
+			DirectX::DXGI_MODE_SCANLINE_ORDER scanlineOrdering;                      /* 0028 */ \
+			std::uint32_t                     isNotWindowed;                         /* 0034 */ \
+			bool                              fullScreen;                            /* 0038 */ \
+			bool                              borderlessDisplay;                     /* 0039 */ \
+			bool                              readOnlyDepth;                         /* 003A */ \
+			bool                              instantiated;                          /* 003B */ \
+			bool                              requestedWindowSizeChange;             /* 003C */ \
+			bool                              unk25;                                 /* 003D */ \
+			std::uint32_t                     newWidth;                              /* 0038 */ \
+			std::uint32_t                     newHeight;                             /* 0040 */ \
+			std::uint32_t                     presentInterval;                       /* 004C */ \
+			ID3D11Device*                     forwarder;                             /* 0050 */ \
+			ID3D11DeviceContext*              context;                               /* 0058 */ \
+			RendererWindow                    renderWindows[32];                     /* 0060 */ \
+			RenderTargetData                  renderTargets[RENDER_TARGET::kTOTAL];  /* 0A60 */
 #else
-#define RUNTIME_DATA_CONTENT                                                                       \
-			std::uint32_t        uiAdapter;                              /* 0010 */                 \
-			std::uint32_t        unk04;                                  /* 0014 - refreshRate? */  \
-			std::uint32_t        unk08;                                  /* 0018 */                 \
-			std::uint32_t        unk0C;                                  /* 001C */                 \
-			std::uint32_t        unk10;                                  /* 0020 */                 \
-			std::uint32_t        unk14;                                  /* 0024 */                 \
-			std::uint32_t        unk18;                                  /* 0028 */                 \
-			std::uint32_t        unk1C;                                  /* 002C */                 \
-			bool                 fullScreen;                             /* 0030 */                 \
-			bool                 borderlessDisplay;                      /* 0031 */                 \
-			bool                 readOnlyDepth;                          /* 0032 */                 \
-			bool                 unk23;                                  /* 0033 */                 \
-			bool                 unk24;                                  /* 0034 */                 \
-			bool                 unk25;                                  /* 0035 */                 \
-			std::uint32_t        unk28;                                  /* 0038 */                 \
-			std::uint32_t        unk30;                                  /* 0040 */                 \
-			std::uint32_t        presentInterval;                        /* 0044 */                 \
-			ID3D11Device*        forwarder;                              /* 0048 */                 \
-			ID3D11DeviceContext* context;                                /* 0050 */                 \
-			RendererWindow       renderWindows[32];                      /* 0058 */                 \
-			RenderTargetData     renderTargets[RENDER_TARGET::kVRTOTAL]; /* 0A58 */
+#define RUNTIME_DATA_CONTENT                                                                     \
+			std::uint32_t                     adapter;                                 /* 0010 */ \
+			DirectX::DXGI_RATIONAL            desiredRefreshRate;                      /* 001C */ \
+			DirectX::DXGI_RATIONAL            actualRefreshRate;                       /* 0020 */ \
+			DirectX::DXGI_MODE_SCALING        scaleMode;                               /* 0024 */ \
+			DirectX::DXGI_MODE_SCANLINE_ORDER scanlineOrdering;                        /* 0028 */ \
+			std::uint32_t                     isNotWindowed;                           /* 0034 */ \
+			bool                              fullScreen;                              /* 0038 */ \
+			bool                              borderlessDisplay;                       /* 0039 */ \
+			bool                              readOnlyDepth;                           /* 003A */ \
+			bool                              instantiated;                            /* 003B */ \
+			bool                              requestedWindowSizeChange;               /* 003C */ \
+			bool                              unk25;                                   /* 003D */ \
+			std::uint32_t                     newWidth;                                /* 0038 */ \
+			std::uint32_t                     newHeight;                               /* 0040 */ \
+			std::uint32_t                     presentInterval;                         /* 004C */ \
+			ID3D11Device*                     forwarder;                               /* 0050 */ \
+			ID3D11DeviceContext*              context;                                 /* 0058 */ \
+			RendererWindow                    renderWindows[32];                       /* 0060 */ \
+			RenderTargetData                  renderTargets[RENDER_TARGET::kVRTOTAL];  /* 0A60 */
 #endif
 			RUNTIME_DATA_CONTENT;
 		};
-#ifndef ENABLE_SKYRIM_VR
+#if !defined(ENABLE_SKYRIM_VR)
 		static_assert(sizeof(RendererData) == 0x1FA8);
 		static_assert(offsetof(RendererData, context) == 0x40);
 		static_assert(offsetof(RendererData, renderTargets) == 0xA48);
@@ -129,6 +126,37 @@ namespace RE
 		static_assert(offsetof(RendererData, context) == 0x40);
 		static_assert(offsetof(RendererData, renderTargets) == 0xA48);
 #endif
+		struct RendererInitOSData
+		{
+			WinAPI::HWND      hwnd;                // 00
+			WinAPI::HINSTANCE instance;            // 08
+			WinAPI::WNDPROC   windowProcFunction;  // 10
+			WinAPI::HICON     icon;                // 18
+			const char*       className;           // 20
+			uint32_t          adapter;             // 28
+			uint32_t          unk2C;               // 2C
+		};
+		static_assert(sizeof(RendererInitOSData) == 0x30);
+
+		struct ScreenSize
+		{
+			uint32_t width;   // 00
+			uint32_t height;  // 04
+		};
+		static_assert(sizeof(ScreenSize) == 0x8);
+
+		struct ApplicationWindowProperties
+		{
+			ScreenSize screenSize;        // 00
+			int        windowX;           // 08
+			int        windowY;           // 0C
+			int        refreshRate;       // 10
+			int        presentInterval;   // 14
+			bool       appFullScreen;     // 18
+			bool       borderlessWindow;  // 19
+			bool       vsync;             // 1A
+		};
+		static_assert(sizeof(ApplicationWindowProperties) == 0x1C);
 
 		class Renderer
 		{
@@ -145,51 +173,93 @@ namespace RE
 
 			[[nodiscard]] inline DepthStencilRuntimeData& GetDepthStencilData() noexcept
 			{
+				if (REL::Module::IsAE())
+					return REL::RelocateMemberIfNewer<DepthStencilRuntimeData>(SKSE::RUNTIME_SSE_1_6_1130, this, 0x1FB8, 0x2018);
+
 				return REL::RelocateMember<DepthStencilRuntimeData>(this, 0x1FB8, 0x21D0);
 			}
 
 			[[nodiscard]] inline const DepthStencilRuntimeData& GetDepthStencilData() const noexcept
 			{
+				if (REL::Module::IsAE())
+					return REL::RelocateMemberIfNewer<DepthStencilRuntimeData>(SKSE::RUNTIME_SSE_1_6_1130, this, 0x1FB8, 0x2018);
+
 				return REL::RelocateMember<DepthStencilRuntimeData>(this, 0x1FB8, 0x21D0);
 			}
 
 			[[nodiscard]] inline RendererData2& GetRendererData() noexcept
 			{
+				if (REL::Module::IsAE())
+					return REL::RelocateMemberIfNewer<RendererData2>(SKSE::RUNTIME_SSE_1_6_1130, this, 0x26D8, 0x2738);
+
 				return REL::RelocateMember<RendererData2>(this, 0x26D8, 0x2E48);
 			}
 
 			[[nodiscard]] inline const RendererData2& GetRendererData() const noexcept
 			{
+				if (REL::Module::IsAE())
+					return REL::RelocateMemberIfNewer<RendererData2>(SKSE::RUNTIME_SSE_1_6_1130, this, 0x26D8, 0x2738);
+
 				return REL::RelocateMember<RendererData2>(this, 0x26D8, 0x2E48);
 			}
+
 			[[nodiscard]] inline SKSE::WinAPI::CRITICAL_SECTION& GetLock() noexcept
 			{
+				if (REL::Module::IsAE())
+					return REL::RelocateMemberIfNewer<SKSE::WinAPI::CRITICAL_SECTION>(SKSE::RUNTIME_SSE_1_6_1130, this, 0x2790, 0x27F0);
+
 				return REL::RelocateMember<SKSE::WinAPI::CRITICAL_SECTION>(this, 0x2790, 0x2F00);
 			}
 
 			[[nodiscard]] inline const SKSE::WinAPI::CRITICAL_SECTION& GetLock() const noexcept
 			{
+				if (REL::Module::IsAE())
+					return REL::RelocateMemberIfNewer<SKSE::WinAPI::CRITICAL_SECTION>(SKSE::RUNTIME_SSE_1_6_1130, this, 0x2790, 0x27F0);
+
 				return REL::RelocateMember<SKSE::WinAPI::CRITICAL_SECTION>(this, 0x2790, 0x2F00);
 			}
 
-			[[nodiscard]] static Renderer* GetSingleton() noexcept;
+			[[nodiscard]] static Renderer*         GetSingleton() noexcept;
+
+			void                                   CreateSwapChain(WinAPI::HWND* window, bool setCurrent);
+			void                                   KillWindow(std::uint32_t windowID);
+			void                                   Lock();
+			void                                   Unlock();
+			void                                   ResizeWindow(std::uint32_t windowID, std::uint32_t width, std::uint32_t height, bool fullscreen, bool borderless);
+			void                                   RequestWindowResize(std::uint32_t width, std::uint32_t height);
+			void                                   SetWindowPosition(std::uint32_t windowID, std::int32_t x, std::int32_t y);
+			void                                   SetWindowActiveState(bool show);
+			void                                   WindowSizeChanged(std::uint32_t windowID);
+			void                                   ResetWindow(std::uint32_t windowID);
+			void                                   UpdateViewPort(std::uint32_t a_unk, std::uint32_t b_unk, bool c_unk);
 
 			[[nodiscard]] NiTexture::RendererData* CreateRenderTexture(std::uint32_t a_width, std::uint32_t a_height);
 			void                                   SaveRenderTargetToFile(RENDER_TARGET a_renderTarget, const char* a_filePath, TextureFileFormat a_textureFileFormat);
+			[[nodiscard]] static RendererData*     GetRendererDataSingleton();
+			[[nodiscard]] static ScreenSize        GetScreenSize();
+			[[nodiscard]] static ID3D11Device*     GetDevice();
+			[[nodiscard]] static RendererWindow*   GetCurrentRenderWindow();
 
+		private:
+			void Begin(std::uint32_t windowID);
+			void Init(RendererInitOSData* a_data, ApplicationWindowProperties* windowProps, WinAPI::HWND window);
+			void End();
+			void Shutdown();
+
+		public:
 			// members
-			std::uint64_t unk000;      // 0000
+			std::uint64_t unk00;       // 0000
 			bool          drawStereo;  // 0008
-#ifndef ENABLE_SKYRIM_VR
+#if !defined(ENABLE_SKYRIM_VR)
 #elif !defined(ENABLE_SKYRIM_AE) && !defined(ENABLE_SKYRIM_SE)
-			std::uint64_t unk010;  // 0010
+			std::uint64_t unk010;      // 0010
 #endif
-			RUNTIME_DATA_CONTENT;  // 0010, VR 18
+			RUNTIME_DATA_CONTENT;      // 0010, VR 18
 		};
-#ifndef ENABLE_SKYRIM_VR
+#if !defined(ENABLE_SKYRIM_VR)
 		static_assert(sizeof(Renderer) == 0x1FB0);
 #elif !defined(ENABLE_SKYRIM_AE) && !defined(ENABLE_SKYRIM_SE)
-		static_assert(sizeof(Renderer) == 0x21B8);
+		static_assert(sizeof(Renderer) == 0x21C0);
 #else
 		static_assert(sizeof(Renderer) == 0x1FB0);
 #endif
